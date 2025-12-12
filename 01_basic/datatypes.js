5ea08e6c7950cf8e10a70a39964205c1ea03574a
shailendra kumawat <kumawatsk2105@gmail.com>
2025-12-12 05:57:41 +0000

    add datatypes.js with examples of JavaScript data types and their usage

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs so alert will not work not browser
+
+console.log(3
+    +
+    3
+); //code readability should be high
+
+let name = "shailendra"; //string datatype
+let age = 25;//number datatype
+
+
+let isLoggedIn = false; //boolean datatype
+let state; //undefined datatype
+
+
+// number => 2 to power 53
+//bigint => larger than 2 to power 53
+//string => "", '', ``
+//boolean => true/false
+//null => standalone value
+//undefined => variable without value
+//symbol => unique identifier //use in to make unique component ;example in figma and react
+
+
+//object => collection of key value pairs
+
+console.log(typeof shailendra); //string
+console.log(typeof null); //object (this is a bug in javascript)
+console.log(typeof undefined); //undefined
+console.log(typeof 25n); //bigint
+console.log(typeof true); //boolean
